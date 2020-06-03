1ec2b5be91b197e93ecc95d3aaca1d6b16803474
AMRI Mohamed <irma.mohamed.ma@gmail.com>
2020-06-03 13:12:16 +0100

    initial commit

---
+++ API_CONFIG/src/test/java/bcp/dc/pmo/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package bcp.dc.pmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
